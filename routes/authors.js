737af5ba52746a38f4c28c5873578d6f49a62f90
Reginork <34095592+Reginork@users.noreply.github.com>
2021-05-01 23:42:22 -0500

    Update authors.js
    Métodos Endpoints

--- routes/authors.js
+++ routes/authors.js
@@ -30,4 +30,44 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Punto 1
+ 
+ router.get('/punto1', async (req, res) => {
+  try {
+    let var1 = {pais: "Colombia", publicados:{$gt:20}};
+    let var2 = {nombre:1, apellido:1, _id:0};
+    const authors = await Author.find(var1, var2);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+
+// Punto2
+ 
+router.get('/punto2', async (req, res) => {
+  try {
+    let var1 = {apellido: null};
+    let var2 = {nombre:1, _id:0};
+    const authors = await Author.find(var1, var2);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Punto3
+ 
+router.get('/punto3', async (req, res) => {
+  try {
+    let var1 ={$or:[{publicados:{$lt:20}},{pais: "Argentina"}]};
+    let var2 = {apellido:1, _id:0};
+    const authors = await Author.find(var1, var2);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 module.exports = router;
